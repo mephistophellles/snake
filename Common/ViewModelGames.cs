97e5b93a299d91a4919b7933e3fff1d473cc6792
ksenich <150047414+mephistophellles@users.noreply.github.com>
2024-11-23 09:15:29 +0500

    Модель представления данных. Суханова

---
+++ Common/ViewModelGames.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class ViewModelGames
+    {
+        public Snakes SnakesPlayers = new Snakes();
+        public Snakes.Point Points = new Snakes.Point();
+        public int Top = 0;
+        public int IdSnake { get; set; }
+    }
+}
