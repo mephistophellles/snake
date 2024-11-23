6b89b6c11ff2941c5a7972002c4e182066d2ecba
ksenich <150047414+mephistophellles@users.noreply.github.com>
2024-11-23 09:08:46 +0500

    Созданный класс змеи. Суханова

---
+++ Common/Snakes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public class Snakes
+    {
+        /// <summary> Класс Point </summary>
+        public class Point
+        {
+            /// <summary> Координата X </summary>
+            public int X { get; set; }
+            /// <summary> Координата Y </summary>
+            public int Y { get; set; }
+
+            public Point(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+
+            public Point() { }
+        }
+
+        /// <summary> Направление движения змеи </summary>
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Start
+        }
+
+        /// <summary> Точки, из которых состоит змея </summary>
+        public List<Point> Points = new List<Point>();
+
+        /// <summary> Направление движения, в котором движется змея </summary>
+        public Direction direction = Direction.Start;
+
+        /// <summary> Переменная, говорящая о том, что игрок закончил игру </summary>
+        public bool GameOver = false;
+    }
+}
