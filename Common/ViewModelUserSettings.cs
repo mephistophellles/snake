c091ef0ac725707fd76cbabf08bfb481ce593815
ksenich <150047414+mephistophellles@users.noreply.github.com>
2024-11-23 09:18:10 +0500

    Модель представления данных пользователя. Суханова

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class ViewModelUserSettings
+    {
+        public string IPAdress { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public int IdSnake = 1;
+    }
+}
