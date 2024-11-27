46fc53df12b3633adc6d702eb4204d0957007825
artem <artem@FATT>
2024-11-27 20:46:11 +0500

    Созданный метод управления змеёй. Черемных

--- Snake_Suhanova/Program.cs
+++ Snake_Suhanova/Program.cs
@@ -43,5 +43,92 @@ namespace Snake_Suhanova
                 }
             }
         }
+        public static void Receiver()
+        {
+            // Создаем UdpClient для чтения входящих данных
+            UdpClient receivingUdpClient = new UdpClient(localPort);
+            IPEndPoint RemoteIpEndPoint = null; // Конечная точка
+
+            try
+            {
+                Console.WriteLine("Команды сервера:");
+
+                // Запускаем бесконечный цикл для обработки входящих сообщений
+                while (true)
+                {
+                    // Ожидание байт данных
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    // Преобразуем и отображаем данные
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Получили команду: " + returnData);
+
+                    // Проверяем, является ли команда стартовой
+                    if (returnData.Contains("/start"))
+                    {
+                        // Делим данные на команду и JSON
+                        string[] dataMessage = returnData.Split('|');
+
+                        // Конвертируем данные в модель
+                        ViewModelUserSettings viewModelUserSettings =
+                            JsonConvert.DeserializeObject<ViewModelUserSettings>(dataMessage[1]);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Подключился пользователь: {viewModelUserSettings.IPAddress}:{viewModelUserSettings.Port}");
+
+                        // Добавляем данные в коллекцию
+                        remoteIPAddress.Add(viewModelUserSettings);
+
+                        // Добавляем змею
+                        viewModelUserSettings.IdSnake = AddSnake();
+
+                        // Создаём запись для игры
+                        viewModelGames[viewModelUserSettings.IdSnake].IdSnake = viewModelUserSettings.IdSnake;
+                    }
+                    else
+                    {
+                        // Если команда не является стартовой
+                        string[] dataMessage = returnData.Split('|');
+
+                        // Конвертируем данные в модель
+                        ViewModelUserSettings viewModelUserSettings =
+                            JsonConvert.DeserializeObject<ViewModelUserSettings>(dataMessage[1]);
+
+                        // Получаем ID игрока
+                        int IdPlayer = remoteIPAddress.FindIndex(x =>
+                            x.IPAddress == viewModelUserSettings.IPAddress &&
+                            x.Port == viewModelUserSettings.Port);
+
+                        // Если игрок найден
+                        if (IdPlayer != -1)
+                        {
+                            // Обрабатываем команды управления
+                            if (dataMessage[0] == "Up" && viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Down)
+                            {
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Up;
+                            }
+                            else if (dataMessage[0] == "Down" && viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Up)
+                            {
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Down;
+                            }
+                            else if (dataMessage[0] == "Left" && viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Right)
+                            {
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Left;
+                            }
+                            else if (dataMessage[0] == "Right" && viewModelGames[IdPlayer].SnakesPlayers.direction != Snakes.Direction.Left)
+                            {
+                                viewModelGames[IdPlayer].SnakesPlayers.direction = Snakes.Direction.Right;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+            }
+        }
     }
 }
