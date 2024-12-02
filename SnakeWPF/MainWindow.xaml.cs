9db1033a68444c68dd57ca8ab034bd26e4cea16c
artem <artem@FATT>
2024-12-02 11:54:56 +0500

    Созданные переменные для клиента игры.Черемных

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +24,15 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public ViewModelUserSettings ViewModelUserSettings=new ViewModelUserSettings();
+        public ViewModelGames ViewModelGames = null;
+        public static IPAddress remoteIPAddress = IPAddress.Parse("127.0.0.1");
+        public static int remotePort = 5001;
+        public Thread tRec;
+        public UdpClient receivingUdpClient;
+        public Pages.Home Home=new Pages.Home();
+        public Pages.Game Game=new Pages.Game();
         public MainWindow()
         {
             InitializeComponent();
